57ab6410eda87f8f41bc381e0809ab3edc2a21e8
Shaun Ryan <shaun_chiburi@hotmail.com>
2024-11-26 18:46:13 +0000

    init

--- src/sqlapp.cpp
+++ src/sqlapp.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-int main(int argc, char *argv[]){
-    std::cout << "sdl2 app" << std::endl;
-    return (0);
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+using namespace std;
+
+int main(int argc, char* args[]) {
+    SDL_Window * window = nullptr;
+
+    SDL_Surface * window_surface = nullptr;
+    SDL_Surface * image_surface = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+
+    window_surface = SDL_GetWindowSurface(window);
+    image_surface = SDL_LoadBMP("image.bmp");
+
+    SDL_BlitSurface(image_surface, NULL, window_surface, NULL);
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyWindow(window);
+    SDL_FreeSurface(image_surface);
+    SDL_Quit();
 }
